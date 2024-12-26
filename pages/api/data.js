356fd842743f4adef2eaff4c7f4c5863226c62af
Enzo Basuil <146545169+SuillyCoder@users.noreply.github.com>
2024-12-26 16:25:10 +0800

    Implemented basic CRUD operations with the HTTP request methods
    Implemented basic CRUD operations with the HTTP request methods

--- pages/api/data.js
+++ pages/api/data.js
@@ -3,18 +3,68 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export default async function reqHandler(req, res) {
+  //READ OPERATION
   if (req.method === "GET") {
     try {
       const apiData = await prisma.apiTable.findMany();
-      //res.status(200).json(apiData); // Use res.json() to send JSON
-      return res.send(apiData);
+      res.status(200).json(apiData); 
     } catch (error) {
       console.error("Error fetching data:", error);
       res.status(500).json({ message: "Failed to fetch data" });
     } finally {
       await prisma.$disconnect();
     }
-  } else {
+  } 
+  //CREATE OPERATION
+  else if (req.method === 'POST') {
+    try {
+      const { name, link, key } = req.body;
+      const apiData = await prisma.apiTable.create({
+        data: {
+          name,
+          link,
+          key,
+        },
+      });
+      res.status(200).json(apiData);
+    } catch (error) {
+      console.error("Error creating data:", error);
+      res.status(500).json({ message: "Failed to create data" });
+    }
+  }
+  //UPDATE OPERATION
+  else if (req.method === 'PUT'){
+    try {
+      const { id, name, link, key } = req.body;
+      const apiData = await prisma.apiTable.update({
+        where: { id },
+        data: {
+          name,
+          link,
+          key,
+        },
+      });
+      res.status(200).json(apiData);
+    } catch (error) {
+      console.error("Error creating data:", error);
+      res.status(500).json({ message: "Failed to create data" });
+    }
+  }
+  //DELETE OPERATION
+  else if (req.method === 'DELETE'){
+    try {
+      const { id } = req.body;
+      const apiData = await prisma.apiTable.delete({
+        where: { id },
+      });
+      res.status(200).json(apiData);
+    } catch (error) {
+      console.error("Error creating data:", error);
+      res.status(500).json({ message: "Failed to create data" });
+    }
+  }
+  //ANYTHING ELSE
+  else {
     res.status(405).json({ message: "Invalid Method" });
   }
 } 
